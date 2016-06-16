1029fbd9c820ee84c3c5b934e0d6450d87b09ac9
fangchen <fangchen@xiaomi.com>
2016-06-16 11:04:55 +0800

    add output template

--- core/calc_value.py
+++ core/calc_value.py
@@ -1,4 +1,5 @@
 import time
+import os
 from price_fetcher import PriceFetcher
 from holdings import Holdings
 
@@ -16,6 +17,18 @@ def read_input():
             holdings.append(line)
     return Holdings(holdings = holdings)
 
+def read_cache():
+    output_file = '../output.txt'
+    if not os.path.isfile(output_file):
+        return None
+    cahce = ''
+    ctime = time.strftime("%Y-%m-%d", time.localtime(int(os.path.getctime(output_file))))
+    today = time.strftime("%Y-%m-%d", time.localtime())
+    if ctime == today:
+        with open(output_file, 'r') as f:
+            cache = f.readline().strip()
+    return cache
+
 def get_value():
     holdings = read_input()
     profit = 0
@@ -28,27 +41,49 @@ def get_value():
         current_price = pf.fetch_current_price()
         if current_price:
             profit += (current_price - operation_price) * operation_count
-        print profit
     setattr(holdings, 'profit', profit)
     return holdings
 
 if __name__ == '__main__':
     start_value, end_value, min_value, max_value = 0, 0, 99999999, -99999999
-    while True:
-        now = time.strftime('%H:%M')
-        h = get_value()
-        profit = getattr(h, 'profit')
-        if profit:
-            if profit < min_value:
-                min_value = profit
-            if profit > max_value:
-                max_value = profit
+    cache = read_cache()
+    if cache:
+        strs = cache.split(',')
+        if len(strs) == 4:
+            start_value, end_value, min_value, max_value = float(strs[0].strip()), float(strs[1].strip()), float(strs[2].strip()), float(strs[3].strip())
+            print start_value, end_value, min_value, max_value
+        else:
+            print 'wrong format'
 
-        if now > '15:01':
-            end_value = profit
-            print 'exit:', start_value, end_value, min_value, max_value
-            break
-        if now < '09:30':
-            start_value = get_value()
-        print start_value, end_value, min_value, max_value
-        time.sleep(5)
+    try:
+        while True:
+            now = time.strftime('%H:%M')
+            if now < '09:27':
+                print "now:", now
+                time.sleep(60)
+                continue
+    
+            h = get_value()
+            profit = getattr(h, 'profit')
+            print profit
+            if profit:
+                if profit < min_value:
+                    min_value = profit
+                if profit > max_value:
+                    max_value = profit
+    
+            if now > '15:01':
+                end_value = profit
+                print 'exit:', start_value, end_value, min_value, max_value
+                break
+            if now < '09:30':
+                start_value = profit
+            print start_value, end_value, min_value, max_value
+            time.sleep(5)
+
+    except (KeyboardInterrupt, SystemExit):
+        with open('../output.txt', 'w') as f:
+            record = '%s,%s,%s,%s' %(start_value, end_value, min_value, max_value)
+            f.write(record + '\n')
+
+
